a02336d260df181739a2f4b93b9545ad6c6270dc
Akash <akash.bhumca23@gmail.com>
2026-09-25 14:05:15 +0530

    Fix component import casing

--- src/pages/Pantry.jsx
+++ src/pages/Pantry.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import IngredientComponent, {
   ingredientEmojis
-} from "./ingredientComponent";
+} from "./IngredientComponent";
 
 function Pantry(props) {
 
